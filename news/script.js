c796d1d53453d53086dadfa77351f67d30de79fe
sangamk04 <99814514+sangamk04@users.noreply.github.com>
2022-02-26 12:53:16 +0530

    news page
    complete

---
+++ news/script.js
@@ -0,0 +1,22 @@
+var a=document.getElementsByClassName("three");
+for(i=0;i<=a.length-1;i++)
+{
+    a[i].addEventListener('mouseenter',first);
+    function first()
+    {
+        this.className='classSec';
+        
+    }
+    a[i].addEventListener('mouseleave',second)
+    function second()
+    {
+        
+        this.className='classThi';
+    }
+}
+
+
+
+
+// ------------------------------------1111111111111-----------------------------------
+
